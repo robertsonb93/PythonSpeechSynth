e6ee71e9b7090f458204a1c27ff57c0de2d1ac5a
BrandonR <robertsonb@uleth.ca>
2017-03-02 21:18:46 -0700

    I think I am making a bit of progress. There is an issue with the 4th parameter (it appears) of the GetConstants function.
    I've tried a few approaches to the typeing of the object being passed, but to no avail, Worse part is it intermittently doesnt segfault, when it doesnt the other parameters have the correct value, but not the 4th param.

--- PlayWith_vocalAPI/PlayWith_vocalAPI.py
+++ PlayWith_vocalAPI/PlayWith_vocalAPI.py
@@ -1,10 +1,17 @@
 import ctypes
+
 import wave #pip install wavefile
+c_int = ctypes.c_int32
+w_char = ctypes.c_wchar_p
+ptr = ctypes.POINTER
+double = ctypes.c_double
+
 
 MyDllObject = ctypes.cdll.LoadLibrary("../../VocalTractLabApi64")
-#it's important to assing the function to an object
+#it's important to assign the function to an object
 #MyFunctionObject = MyDllObject.MyFunctionName
-Initialize = MyDllObject.vtlInitialize #int
+
+Initialize = MyDllObject.vtlInitialize 
 Close = MyDllObject.vtlClose
 GetVersion = MyDllObject.vtlGetVersion
 GetConstants = MyDllObject.vtlGetConstants
@@ -22,38 +29,38 @@ GesToWav = MyDllObject.vtlGesToWav #Use this function to turn a gestural Score i
 
 
 #define the types that your C# function return
-#MyFunctionObject.restype = ctypes.c_wchar_p
-Initialize.restype = ctypes.c_int
-Close.restype = ctypes.c_void_p
-GetVersion.restype = ctypes.c_void_p
-GetConstants.restype = ctypes.c_void_p
-GetTractParamInfo.restype = ctypes.c_void_p
-GetGlottisParamInfo.restype = ctypes.c_void_p
-GetTractParams.restype = ctypes.c_int
-GetTransferFunction.restype = ctypes.c_void_p
-SynthBlock.restype = ctypes.c_void_p
-TubeSynthesisReset.restype =  ctypes.c_void_p
-TubeSynthesisAdd.restype = ctypes.c_void_p
-ApiTest1.restype = ctypes.c_void_p
-ApiTest2.restype = ctypes.c_void_p
-GesToWav.restype = ctypes.c_int
+#MyFunctionObject.restype = w_char
+Initialize.restype = c_int
+Close.restype = None
+GetVersion.restype = None
+GetConstants.restype = None
+GetTractParamInfo.restype = None
+GetGlottisParamInfo.restype = None
+GetTractParams.restype = c_int
+GetTransferFunction.restype = None
+SynthBlock.restype = None
+TubeSynthesisReset.restype =  None
+TubeSynthesisAdd.restype = None
+ApiTest1.restype = None
+ApiTest2.restype = None
+GesToWav.restype = c_int
 
 
 #define the types that your C# function will use as arguments
-#MyFunctionObject.argtypes = [ctypes.c_wchar_p]
+#MyFunctionObject.argtypes = [w_char]
 Initialize.argtype = [ctypes.c_char_p]
 Close.argtype = [];
-GetVersion.argtype = [ctypes.c_wchar_p]
-GetConstants.argtype = [ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int)]
-GetTractParamInfo.argtype = [ctypes.c_wchar_p,ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double)]
-GetGlottisParamInfo.argtype = [ctypes.c_wchar_p,ctypes.POINTER(ctypes.c_double)]
-GetTractParams.argtype = [ctypes.c_wchar_p,ctypes.POINTER(ctypes.c_double)]
-GetTransferFunction.argtype = [ctypes.POINTER(ctypes.c_double),ctypes.c_int,ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double)]
-SynthBlock.argtype = [ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.c_wchar_p,ctypes.c_int,ctypes.c_double,ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_int)]
-TubeSynthesisReset.argtype = [ctypes.c_int, ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.c_wchar_p,ctypes.c_double,ctypes.c_double,ctypes.c_double,ctypes.POINTER(ctypes.c_double)]
-ApiTest1.argtype = [ctypes.c_wchar_p, ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_int)]
-ApiTest2.argtype = [ctypes.c_wchar_p, ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_int)]
-GesToWav.argtype = [ctypes.c_wchar_p, ctypes.c_wchar_p,ctypes.c_wchar_p,ctypes.c_wchar_p]
+GetVersion.argtype = [w_char]
+GetConstants.argtype = [ptr(c_int),ptr(c_int),ptr(c_int),ptr(c_int)]
+GetTractParamInfo.argtype = [w_char,ptr(double),ptr(double),ptr(double)]
+GetGlottisParamInfo.argtype = [w_char,ptr(double)]
+GetTractParams.argtype = [w_char,ptr(double)]
+GetTransferFunction.argtype = [ptr(double),c_int,ptr(double),ptr(double)]
+SynthBlock.argtype = [ptr(double),ptr(double),ptr(double),w_char,c_int,double,ptr(double),ptr(c_int)]
+TubeSynthesisReset.argtype = [c_int, ptr(double),ptr(double),ptr(double),w_char,double,double,double,ptr(double)]
+ApiTest1.argtype = [w_char, ptr(double),ptr(c_int)]
+ApiTest2.argtype = [w_char, ptr(double),ptr(c_int)]
+GesToWav.argtype = [w_char, w_char,w_char,w_char]
 
 def wave_to_double(filename):
     w = wave.open(filename,'r')
@@ -68,25 +75,48 @@ speaker = ctypes.c_char_p(s)
 audio = wave_to_double("..\\..\\VTL2.1\\Example5-Hallo-synth.wav");
 
 #Audio is a list, convert it to an array of doubles 
-audioPtr = (ctypes.c_double * len(audio))(*audio)
-
-
-#ApiTest1(speaker, audioPtr, len(audio))#currently throwing an unhandled OS exception
+audioPtr = (double * len(audio))(*audio)
+#ApiTest1(speaker, ctypes.byref(audioPtr), audioPtr._length_)#currently throwing an unhandled OS exception
 
 
 print(Initialize(speaker)) # 0 means success, >0 means error
 print(Close());
-print("Get Constants")
-audioSamplingRate_Int = ctypes.POINTER(ctypes.c_int(1))#So Frustrating... this is not a  ctypes type......
-numTubeSections_Int =  ctypes.POINTER(ctypes.c_int(1))
-numVocalTractParams = ctypes.POINTER(ctypes.c_int(1))
-numGlottisParams =  ctypes.POINTER(ctypes.c_int(1))
-GetConstants(audioSamplingRate_Int,numTubeSections_Int,numVocalTractParams,numGlottisParams);
-print(audioSamplingRate_Int)
-print(numTubeSections_Int)
-print(numVocalTractParams)
-print(numGlottisParams);
 
-#I think the way to work with this API for ML purposes is going to be by loading a speaker, then modifiying it using TubesynthesisAdD, if 
+print("Get Version")
+
+version = ctypes.create_string_buffer(64)
+GetVersion(ctypes.byref(version)) #Doing this causes the getConstants() to stop segfaulting. This Python-C API deal is infuriating
+print(version.value)
+
+print("Get Constants Pre")
+audioSamplingRate_Int =c_int(1)
+numTubeSections_Int =  c_int(1)
+numVocalTractParams = c_int(1)
+numGlottisParams =  c_int(1) #I have tried as both, being a single integer, with it going as a long, int, int32, 
+numGlottisParams = (c_int * 64)(99)#After trying the single values, tried handing it some arrays as both double and integer sizes 1,16,32,64
+
+print(audioSamplingRate_Int.value)
+print(numTubeSections_Int.value)
+print(numVocalTractParams.value)
+print(numGlottisParams[0])
+
+
+
+GetConstants(ctypes.byref(audioSamplingRate_Int), 
+             ctypes.byref(numTubeSections_Int),
+             ctypes.byref(numVocalTractParams),
+             ctypes.byref(numGlottisParams)); #Well it was working, till I decided to hit reset.... then it changed its mind..
+
+
+print("Get Constants Post")#IF I keep mashing the reset/rerun button, sometimes i get to see these values except for numGlottisParams
+print(audioSamplingRate_Int.value)
+print(numTubeSections_Int.value)
+print(numVocalTractParams.value)
+print(numGlottisParams[0])#when these do pass through, they all have the correct values except for numGlottisParams(returns zero, not 6)
+
+
+
+
+#I think the way to work with this API for ML purposes is going to be by loading a speaker, then modifiying it using TubesynthesisAdd, if 
 #i understand it correctly(though I probably dont). 
 #Perhaps we start with vtlSynthBlock and create the "model" to be synthesized, theen use the tubesenthesis mentioned above
