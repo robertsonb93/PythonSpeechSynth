2e6abcd6dd7a2f6a7413507142da2114bb2ef558
BrandonR <robertsonb@uleth.ca>
2017-03-12 14:39:09 -0600

    Things are going better now I think. Aside from the latest function we are trying to test/use, it asks for the concatenated vector of all glottis parameter vectors, but i dont think i have a single one, unless that is glottisNames or something along those lines.

--- PlayWith_vocalAPI/PlayWith_vocalAPI.py
+++ PlayWith_vocalAPI/PlayWith_vocalAPI.py
@@ -110,13 +110,11 @@ print(version.value)
 audioSamplingRate_Int =c_int(1)
 numTubeSections_Int =  c_int(1)
 numVocalTractParams = c_int(1)
-numGlottisParams =  c_int(1) #I have tried as being a single integer, with it going as a long, int, int32, 
-
-GetConstants(ctypes.byref(audioSamplingRate_Int), 
-             ctypes.byref(numTubeSections_Int),
-           ctypes.byref(numVocalTractParams),
-             ctypes.byref(numGlottisParams),#I have tried re-ordering the params to ensure there isnt a type difference, but to no avail.
-             ) #Well it was working, till I decided to hit reset.... then it changed its mind.. Intermittent SegFaults
+numGlottisParams =  c_int(1) 
+GetConstants(ref(audioSamplingRate_Int), 
+             ref(numTubeSections_Int),
+          ref(numVocalTractParams),
+            ref(numGlottisParams))
 
 
 #// ****************************************************************************
@@ -127,13 +125,13 @@ GetConstants(ctypes.byref(audioSamplingRate_Int),
 #// by spaces. This string should have at least 10*numParams elements.
 #// ****************************************************************************
 
-aSize = numGlottisParams.value *10; 
-names = (c_char *aSize)(0)
+aSize = numGlottisParams.value *11; 
+tractNames = (c_char *aSize)(0)
 paramMin = (double * aSize)(0)
 paramMax = (double * aSize)(0)
 paramNeutral = (double * aSize)(0)
 
-GetTractParamInfo(names,paramMin,paramMax,paramNeutral);#If you dont have the size large enough, python will just crash. no erros just die.
+GetTractParamInfo(tractNames,paramMin,paramMax,paramNeutral);#If you dont have the size large enough, python will just crash. no erros just die.
 
 
 
@@ -145,14 +143,13 @@ GetTractParamInfo(names,paramMin,paramMax,paramNeutral);#If you dont have the si
 #// by spaces. This string should have at least 10*numParams elements.
 #// ****************************************************************************
 
-
 aSize = numGlottisParams.value * 10;
-names = (c_char *aSize)(0)
+glottisNames = (c_char *aSize)(0)
 paramMin = (double * aSize)(0)
 paramMax = (double * aSize)(0)
 paramNeutral = (double * aSize)(0)
 
-GetGlottisParamInfo(names,paramMin,paramMax,paramNeutral)
+GetGlottisParamInfo(glottisNames,paramMin,paramMax,paramNeutral)
 
 
 
@@ -164,11 +161,97 @@ GetGlottisParamInfo(names,paramMin,paramMax,paramNeutral)
 #// Returns 0 in the case of success, or 1 if the shape is not defined.
 #// ****************************************************************************
 
-size = numVocalTractParams.value*25;
-shapeName = (ctypes.c_wchar * size)("a")
-param = (double * size)()
+#Notice VocalTractParameters and given shape, we need a shape name! lets try using names from the GetTractParamInfo
+size = numVocalTractParams.value;
 
-ret = GetTractParamInfo(shapeName,param)#And we segfault! I believe the issue is with shapeName, the speaker file has fields labelled shapeName and they refer to "a","e","i" ...
+paramList = list();
+for i in range(0,tractNames._length_):
+    shapeName = ctypes.c_char_p(tractNames[i]) 
+    param = (double * size)()
+    
+    ret = GetTractParams(shapeName,param) ## I am suspicious this is currently broken...
+    if(ret <1):#I wonder if this is the right way to do this.
+        for p in range(0,size):
+             paramList.append(param[p]);
+    #print(ret)#This produces a mix of true and false, so i guess its working
+
+
+#// ****************************************************************************
+#// Calculates the volume velocity transfer function of the vocal tract between 
+#// the glottis and the lips for the given vector of vocal tract parameters and
+#// returns the spectrum in terms of magnitude and phase.
+#// Parameters (in/out):
+#// o tractParams (in): Is a vector of vocal tract parameters with 
+#//     numVocalTractParams elements.
+#// o numSpectrumSamples (in): The number of samples (points) in the requested 
+#//     spectrum. This number of samples includes the negative frequencies and
+#//     also determines the frequency spacing of the returned magnitude and
+#//     phase vectors. The frequency spacing is 
+#//     deltaFreq = SAMPLING_RATE / numSpectrumSamples.
+#//     For example, with the sampling rate of 22050 Hz and 
+#//     numSpectrumSamples = 512, the returned magnitude and phase values are 
+#//     at the frequencies 0.0, 43.07, 86.13, 129.2, ... Hz.
+#// o magnitude (out): Vector of spectral magnitudes at equally spaced discrete 
+#//     frequencies. This vector mus have at least numSpectrumSamples elements.
+#// o phase_rad (out): Vector of the spectral phase in radians at equally 
+#//     spaced discrete frequencies. This vector mus have at least 
+#//     numSpectrumSamples elements.
+#// ****************************************************************************
+
+#dbl*, int, dbl*, dbl*
+#use the tractParams that is given by GetTractParams
+tractParams = param;
+numSamples = c_int(10);
+magnitude = (double * numSamples.value)();
+phaseRad = (double * numSamples.value)();
+
+GetTransferFunction(tractParams,numSamples,magnitude,phaseRad)
+
+
+#// ****************************************************************************
+#// Synthesize speech with a given sequence of vocal tract model states and 
+#// glottis model states, and return the corresponding sequence of tube
+#// area functions and the audio signal.
+#// Parameters (in/out):
+#// o tractParams (in): Is a concatenation of vocal tract parameter vectors
+#//     with the total length of (numVocalTractParams*numFrames) elements.
+#// o glottisParams (in): Is a concatenation of glottis parameter vectors
+#//     with the total length of (numGlottisParams*numFrames) elements.
+#// o tubeArea_cm2 (out): Is a concatenation of vocal tract area functions that
+#//     result from the vocal tract computations. Reserve (numTubeSections*numFrames)
+#//     elements for this vector!
+#// o tubeArticulator (out): Is a concatenation of articulator vectors.
+#//     Each single vector corresponds to one area function and contains the 
+#//     articulators for each tube section (cf. vtlTubeSynthesisAdd(...)).
+#//     Reserve (numTubeSections*numFrames) elements for this vector!
+#// o numFrames (in): Number of successive states of the glottis and vocal tract
+#//     that are going to be concatenated.
+#// o frameRate_Hz (in): The number of frames (states) per second.
+#// o audio (out): The resulting audio signal with sample values in the range 
+#//     [-1, +1] and with the sampling rate audioSamplingRate. Reserve enough
+#//     elements for the samples at the given frame rate, sampling rate, and
+#//     number of frames!
+#// o numAudioSamples (out): The number of audio samples written into the
+#//     audio array.
+#//
+#// Returns 0 in case of success, otherwise an error code > 0.
+#// ****************************************************************************
+
+#What is numFrames? I think a frame is a sample, so how many samples do we want.
+#what is frameRate? how many samples per second does the audio play??
+numFrames = len(audio);
+frameRate = audioSamplingRate_Int.value;
+
+tractParams2 = (double * (numFrames*numVocalTractParams.value))()
+i=0;
+for tp in tractParamList:
+    tractParams2[i] = tp;
+
+#glottisParams2 = it says a concatentaion of glottis param vectors,, but we dont have these??? Do we make them?
+
+
+
+
 
 print(Close());
 
