0e241c8cd61cdc0c4bfb829bed139d8b436e96d3
BrandonR <robertsonb@uleth.ca>
2017-07-28 15:45:22 -0600

    With a few more changes now to the Tensorboard, and have tried approaching the variable length issue with the input. This is Last commit before I commit myself to turning this into a RNN instead.

--- PlayWith_vocalAPI/MLAttempt.py
+++ PlayWith_vocalAPI/MLAttempt.py
@@ -11,7 +11,7 @@ import time
 
 
 #********************************* DEFINITIONS************************************#
-#*********************************************************************************#
+#*********************************************************************************# 
 
 #Will iterate through the list of csv training files, and create batches of training data
 #the csv columns are organized as the output first (about 144 columns) then the audio input
@@ -31,6 +31,7 @@ def readFilesnBatch(files,batchSize,inSize,outSize):
     min_after_dequeue = 1000
     capacity = min_after_dequeue+3 * batchSize
     audioBatch,paramBatch = tf.train.shuffle_batch([audio,params],batch_size=batchSize,capacity=capacity,min_after_dequeue=min_after_dequeue,allow_smaller_final_batch=True)
+   # audioBatch,paramBatch = tf.train.batch([audio,params],batch_size=batchSize,capacity = capacity,allow_smaller_final_batch=True,dynamic_pad=True)
     return audio,params,audioBatch,paramBatch
 
 #Given a audiofile name, and the number of samples to split the audio up into, will
@@ -57,32 +58,39 @@ def weight_variable(shape):
   initial = tf.random_normal(shape, stddev=0.1)
   return tf.Variable(initial)
 
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape=shape)
+    return tf.Variable(initial)
 
 def weight_summaries(var):
+  sumry = list()
   #Attach a lot of summaries to a Tensor (for TensorBoard visualization).#
   with tf.name_scope('summaries'):
     mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean', mean)
+    sumry.append(tf.summary.scalar('mean', mean))
     with tf.name_scope('var_stddev'):
       variance = tf.reduce_mean(tf.square(var-mean))
       stddev = tf.sqrt(variance)
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('variance',variance)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
-    tf.summary.histogram('Weight_Values', var)
+    sumry.append(tf.summary.scalar('stddev', stddev))
+    sumry.append(tf.summary.scalar('variance',variance))
+    sumry.append(tf.summary.scalar('max', tf.reduce_max(var)))
+    sumry.append(tf.summary.scalar('min', tf.reduce_min(var)))
+    sumry.append(tf.summary.histogram('Weight_Values', var))
+  return sumry
 
 def variable_summary(var):
+    sumry = list()
     with tf.name_scope('summaries'):
         mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean',mean)
+        sumry.append( tf.summary.scalar('mean',mean))
         with tf.name_scope('var_stddev'):
             variance = tf.reduce_mean(tf.square(var-mean))
             stddev = tf.sqrt(variance)
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('variance',variance)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
+        sumry.append(tf.summary.scalar('stddev', stddev))
+        sumry.append(tf.summary.scalar('variance',variance))
+        sumry.append(tf.summary.scalar('max', tf.reduce_max(var)))
+        sumry.append(tf.summary.scalar('min', tf.reduce_min(var)))
+    return sumry
 
 #I hope this is correct
 #Review how to properly build this
@@ -90,32 +98,44 @@ def variable_summary(var):
 #X is the input Data, W[i] are the weights of the i'th layer
 #yHat is the prediction/regression result of the network
 def forwardprop(X, W):
+    sumry = list()
     with tf.name_scope("InLayer"):
-        h = tf.nn.sigmoid(tf.matmul(X, W[0]),name = "InLayer")
-        inputLayer = tf.matmul(X,W[0])
-        inputLayerAct = tf.nn.sigmoid(inputLayer)
-        weight_summaries(W[0])
-    tf.summary.histogram("InputLayer",inputLayer)
-    tf.summary.histogram("InputLayer_Activation",inputLayerAct)
+        with tf.name_scope("Weights"):
+            inputLayer = tf.matmul(X,W[0])
+            h = tf.nn.sigmoid(inputLayer,name = "InLayer")
+            sumry += weight_summaries(W[0])
+            sumry.append(tf.summary.histogram("InputLayer",inputLayer))
+            sumry.append(tf.summary.histogram("InputLayer_Activation",h))#This is the activation
+        with tf.name_scope("InputBias"):
+            biasSize = (W[0]).get_shape()[1].value
+            b = bias_variable([biasSize])
+            h += b
+            sumry.append(tf.summary.histogram("InputBias",b))
 
     count = 0;
     for w in W[1:len(W)-1]:       
-        name = "H-Layer-Weights_" + str(count)
+        name = "H-Layer_" + str(count)
         count +=1
         with tf.name_scope(name):
-            layer = tf.matmul(h,w)
-            h = tf.nn.sigmoid(layer, name = name)
-            weight_summaries(w)
-        tf.summary.histogram(name, layer)
-        tf.summary.histogram(name + "_activation",h)
+             with tf.name_scope("Weights"):
+                layer = tf.matmul(h,w)
+                h = tf.nn.sigmoid(layer, name = name)
+                sumry += weight_summaries(w)
+                sumry.append(tf.summary.histogram(name, layer))
+                sumry.append(tf.summary.histogram(name + "_activation",h))
+             with tf.name_scope("Bias"):
+                biasSize = w.get_shape()[1].value #since W has [input,output]
+                b = bias_variable([biasSize])
+                h += b
+                sumry.append(tf.summary.histogram(name+'Bias',b))
     
     with tf.name_scope("OutputLayer"):
         yhat = tf.matmul(h,W[len(W)-1],name = "OutputLayer")
 
-    weight_summaries(W[len(W)-1])
-    tf.summary.histogram("OutputLayer",yhat)
+        sumry += weight_summaries(W[len(W)-1])
+        sumry.append(tf.summary.histogram("OutputLayer",yhat))
 
-    return yhat
+    return yhat, sumry
 
 #The scales of the elements in the output are quite varied
 #This is an attempt to normalize them for the networks sake 
@@ -155,6 +175,7 @@ def normalizeOut(batch,batchSz,tubeCount,VTPCount,glotcount):
         for a in range(2):
             batch[b,i] = (batch[b,i] - aspMin) / (0 - aspMin)
             i = i+1
+        #TODO: Normalize Framerate
 
 #Undo the previous normalization so that the values can be handed to the synthesizer with unabashed values
 def denormalizeOut(batch,batchSz,tubeCount,VTPCount,glotcount):
@@ -189,7 +210,7 @@ def denormalizeOut(batch,batchSz,tubeCount,VTPCount,glotcount):
         for a in range(2):
             batch[b,i] = (batch[b,i] * (0 - aspMin) + aspMin)
             i = i+1
-    
+    #TODO: Normalize Framerate
 
 
 
@@ -209,7 +230,7 @@ def denormalizeOut(batch,batchSz,tubeCount,VTPCount,glotcount):
 #The input to the learner would be the desired sound
 #The feedback/error is what is generated by the VTL model from the networks given parameters.
 
-
+networkParams = list()
 spkr ="test1.speaker"
 framerate = 4
 
@@ -241,39 +262,60 @@ outSize +=1 #For the added framerate
     #Here We give TF a list of the files it can read to get our values from, 
     #the proceed to organize them into batches that are sent to TF
 maxBatchSz = 100
+maxTestBatchSz = 10
 trainFiles = list() #Select which files we want to train off of
-for i in range(50,300): #using files 50-299 as they they are currently at size framerate 4
+testFiles= list()
+trainStart = 50
+trainEnd = 250
+testStart = 250
+testEnd = 300
+
+for i in range(trainStart,trainEnd): #using files 50-299 as they they are currently at size framerate 4
       trainFiles.append((prefix + str(i) + postfix))
-audio,params,audioBatch,paramBatch= readFilesnBatch(trainFiles,maxBatchSz,inSize,outSize)  
+for i in range(testStart,testEnd):
+      testFiles.append((prefix+str(i)+postfix))
+
+_,_,audioBatch,paramBatch= readFilesnBatch(trainFiles,maxBatchSz,inSize,outSize)
+_,_,testAudioBattch,testParamBatch = readFilesnBatch(testFiles,maxTestBatchSz,inSize,outSize)
+
 print("Batching complete")
 
 t_start = time.process_time()
 with tf.Session() as sess:
 
+    trainSummaries = list()
+    testSummaries = list()
+    
     print("Entered Session")
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
     
     with tf.name_scope("Input_Audio"):
         inputAudio = tf.placeholder(tf.float32,shape=[None,inSize],name = "inputAudio")
+        trainSummaries.append(tf.summary.audio("Input_Audio",inputAudio,srate,10))
     with tf.name_scope("Actual_output"):
-        outActual = tf.placeholder(tf.float32,shape=[None,outSize],name = "outActual")
-    tf.summary.audio("Input_Audio",inputAudio,srate,10)
-    tf.summary.histogram("Real_output",outActual)
+        outActual = tf.placeholder(tf.float32,shape=[None,outSize],name = "outActual")       
+        trainSummaries.append(tf.summary.histogram("Real_output_Train",outActual))
+        testSummaries.append(tf.summary.histogram("Real_output_Test",outActual))
 
     #create Weights (this is basically the network)
+    networkDepth = 20
     h_size = inSize
     W = list()
-    W.append(weight_variable((inSize,h_size)))
-    for s in range(2):
+    W.append(weight_variable((inSize,h_size)))  
+    for s in range(networkDepth):
         W.append(weight_variable((h_size,h_size)))
     W.append(weight_variable((h_size,outSize)))
     print("Weights Generated")
 
     #Forward propagation(How to calculate from input to output)
     with tf.name_scope("Generated_Output"):
-        outEstimate = forwardprop(inputAudio, W)
-    tf.summary.histogram("NetworkOutput",outEstimate)
+        outEstimate,sumry = forwardprop(inputAudio, W)
+        trainSummaries.append(sumry)
+        testSummaries.append(sumry)
+
+    trainSummaries.append(tf.summary.histogram("NetworkOutput_Train",outEstimate))
+    testSummaries.append(tf.summary.histogram("NetworkOutput_Test",outEstimate))
 
 
     #Back Progoation(How to correct the Error from the estimate)
@@ -283,104 +325,87 @@ with tf.Session() as sess:
         cost =tf.reduce_mean(costSplit)#Back prop is here 
         costListTensor = tf.placeholder(tf.float32,[None],name = "Cost_History")
         variable_summary(costListTensor)
-        tf.summary.scalar("cost",cost)
+        trainSummaries.append(tf.summary.scalar("cost_while_training",cost))
+        testSummaries.append(tf.summary.scalar("cost_while_testing",cost))
+    print("Completed Cost")
+
     
 
     with tf.name_scope("GD_Trainer"):
-        train_step = tf.train.AdamOptimizer(0.9).minimize(cost)
+        learningRate = 0.9
+        train_step = tf.train.AdamOptimizer(learningRate).minimize(cost)
  
 
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    print("Session Init complete")
 
-    #AC = 0 #Average Cost, 
-    #ACL = list() #AverageCost List
-    #costList = list()
-    #costListSplit = list()
     epochs = 10
+    testFreq = 100
     
-    mergedSummaries = tf.summary.merge_all()
-    writer = tf.summary.FileWriter('.\TensorBoard',sess.graph)
+    #textTensor = tf.placeholder(networkParams,tf.string,name = "Network_Parameters")
+    textTensor = tf.placeholder(tf.string,shape = [14],name = "Network_Parameters")
+    textSumry = tf.summary.text(name = "Network_Parameter_Summary",tensor = textTensor)
+    networkParams.append(("FrameRate:"+str(framerate)))
+    networkParams.append(("Samplerate:"+str(srate)))
+    networkParams.append(("InputSize:"+str(inSize)))
+    networkParams.append(("OutputSize:"+str(outSize)))
+    networkParams.append(("Training Batch Size:"+str(maxBatchSz)))
+    networkParams.append(("Testing Batch Size:"+str(maxTestBatchSz)))
+    networkParams.append(("Training files:"+str(trainStart)+"-"+str(trainEnd)))
+    networkParams.append(("Testing files:"+str(testStart)+"-"+str(testEnd)))
+    networkParams.append(("Hidden Layer Width:"+str(h_size)))
+    networkParams.append(("Hidden Layers: "+str(networkDepth)))
+    networkParams.append(("Learning Rate:"+str(learningRate)))
+    networkParams.append(("Epochs Run:"+str(epochs)))
+    networkParams.append(("testFreq:"+str(testFreq)))
+    
+
+    trainSumry = tf.summary.merge(trainSummaries)
+    testSumry = tf.summary.merge(testSummaries)
+    textSumry = tf.summary.merge([textSumry],name = "Network_Text_summary")
+    writer = tf.summary.FileWriter('.\TensorBoard',sess.graph,filename_suffix = "Run1")
+    saver = tf.train.Saver()
+    print("Completed Summaries")
+
+    sess.run(tf.local_variables_initializer())
+    init = tf.global_variables_initializer()
+    sess.run(init)   
+    print("Session Init complete")
 
     for i in range(epochs):
         print(i," of ",epochs)
-        x,y,currBatchSize = sess.run([audioBatch,paramBatch,tf.shape(paramBatch)[0]])# here we are assigning batchsize components to the None Dimension for the x,y
-        print("currBatchSize ",currBatchSize)
-        normalizeOut(y,currBatchSize,tubecount,vtcount,glotcount)
 
-        if i <1: #This is here to push the initial cost into the cost list, then it can be summarized into the tensor below
-            Cst = sess.run(cost,feed_dict={inputAudio: x, outActual: y})
-            costList[0] = Cst
+        if (i%testFreq):
+            x,y,currBatchSize = sess.run([audioBatch,paramBatch,tf.shape(paramBatch)[0]])# here we are assigning batchsize components to the None Dimension for the x,y
+            normalizeOut(y,currBatchSize,tubecount,vtcount,glotcount)
+
+            if i <1: #This is here to push the initial cost into the cost list, then it can be summarized into the tensor below
+                Cst = sess.run(cost,feed_dict={inputAudio: x, outActual: y})
+                costList[0] = Cst
                     
-        sessList = [costSplit, cost, train_step, mergedSummaries]
-        CstSplit,Cst,_,summary = sess.run(sessList, 
-                                          feed_dict={inputAudio: x, outActual: y, costListTensor: costList})
+            sessList = [costSplit, cost, train_step, trainSumry]
+            CstSplit,Cst,_,sumry = sess.run(sessList, 
+                                          feed_dict={inputAudio: x, outActual: y, costListTensor: costList},
+                                          options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
+                                          run_metadata=tf.RunMetadata()
+                                          )
+            costList[i%10] = Cst
+        else:
+            xTest,yTest = sess.run([testAudioBattch,testParamBatch])
+            normalizeOut(yTest,maxTestBatchSz,tubecount,vtcount,glotcount)
+            sessList = [cost,testSumry]
+            Cst,sumry = sess.run(sessList,feed_dict = {inputAudio:xTest, outActual: yTest})
+            ##saver.save(sess,'.\TensorBoard\checkpoints',global_step= i)
 
-        costList[i%10] = Cst
-        writer.add_summary(summary,i)
-        
-        #AC = (Cst + (i+1%10)*AC)/(i+1%10)#Average Cost, keep track of the last 10 iterations since the initial learning will dilute the results as time goes on
-        #if((i%10) == 0):
-        #    ACL.append(AC) #AverageCost List
-        #    print("Average Cost: ",AC)
-        #    AC = 0
-            
-        
-        #costList.append(Cst)
-        #if(i > epochs-11): #I am putting this here so we can see what the variance looks like when we have had a chance to learn something.
-        #    costListSplit.append(CstSplit)
-
-        #if (i>epochs-2):
-        #    #Calc the variance of the error here, 
-        #    muList = [0] * outSize
-        #    for c in range(len(costListSplit)):#number of epochs
-        #       for i in range(len(costListSplit[c])):#size of the batch
-        #           for e in range(len(costListSplit[c][i])):#these are the values in each example
-        #               muList[e] += costListSplit[c][i][e]
-
-        #    for i in range(len(muList)):
-        #        muList[i] /= (len(costListSplit) * len(costListSplit[0])) #divide by the number of examples for each value, that is epochs*batchSize
-
-        #    sigmaList = [0] * outSize
-        #    for i in range(len(muList)):#The number of values
-        #       for c in range(len(costListSplit)):#The number of Epochs
-        #           for e in range(len(costListSplit[c])):#the sizse of the batch
-        #            sigmaList[i] += (costListSplit[c][e][i] - muList[i])**2
-        #       sigmaList[i] /= (len(costListSplit) * len(costListSplit[c])) #This is the SigmaSquared (variance) of our costs , its plotted below
-             
-
-        #     #Here we will sample an single case, and compare how off it is from its true value, 
-        #    outEst = sess.run(outEstimate,feed_dict={inputAudio: x})
-        #    denormalizeOut(outEst,currBatchSize,tubecount,vtcount,glotcount)
-        #    denormalizeOut(y,currBatchSize,tubecount,vtcount,glotcount)
-
-        #    est = outEst[0,:]
-        #    act = y[0,:]
-        #    diff = list()
-        #    for e in range(len(est)):
-        #        diff.append(act[e]-est[e])
-        #    indices = range(len(diff))
-        #    width = 0.8
-        #    indices2 = [i+0.25*width for i in indices]
-        #    indices3 = [i+0.25*width for i in indices2]
-
-        #    plt.bar(indices,act,width=width,color='b',label = "Actual Values")
-        #    plt.bar(indices2,est,width=0.5*width,color = 'r',label = "Regressed Values")
-        #    plt.bar(indices3,diff,width=0.25*width,color='g',label = "Cost")
-        #    plt.title("Single comparison of Actual vs estimated Output (denormalized)")
-        #    plt.legend()
-        #    plt.show()
-            
-        #    plt.bar(indices,sigmaList,label = "Cost Variance")
-        #    plt.title("Variance of Cost")
-        #    plt.legend()
-        #    plt.show()
-           
-
+        writer.add_summary(sumry,i)
+                           
+        #TODO: Create a better test where we generate the audio and then can compare it to the inputAudio, perhaps every 1000 steps
                              
     t_end = time.process_time()
+    tme = t_end-t_start
     print("Total Training Network time = ",t_end-t_start)
+    networkParams.append(("Time Running:"+str(tme)))
+
+    #sumry = sess.run([textSumry],feed_dict= {textTensor:networkParams})
+    #writer.add_summary(sumry,0)
 
 #Try giving the network a unadultereed audio file input, and then see how the model and network stack up to replicating it.
 #Open and split the test audio into even size chunks
@@ -405,12 +430,6 @@ with tf.Session() as sess:
 
     coord.request_stop()
     coord.join(threads)
-
-#plt.plot(costList,'b')
-#plt.plot(ACL,'r')
-#plt.title("Cost and the Average Cost per 10 steps")
-#plt.show()
-
 
 
 
